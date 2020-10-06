47db9529076396585e6e135d8a0a07f887c06daf
powerfulyang <i@powerfulyang.com>
2020-10-06 20:50:47 +0800

    refactor: custome logger

---
+++ src/common/logger/app.logger.ts
@@ -0,0 +1,46 @@
+import { Injectable, LoggerService, Scope } from '@nestjs/common';
+import winston, { format, Logger } from 'winston';
+import { __prod__ } from '@powerfulyang/utils';
+
+const { combine, timestamp, label, prettyPrint } = format;
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class AppLogger implements LoggerService {
+    private logger: Logger;
+
+    setContext(context: string) {
+        this.initializeLogger(context);
+    }
+
+    initializeLogger(context: string) {
+        this.logger = winston.createLogger({
+            level: (__prod__ && 'info') || 'debug',
+            format: combine(
+                label({ label: context }),
+                timestamp(),
+                prettyPrint(),
+            ),
+            transports: [new winston.transports.Console()],
+        });
+    }
+
+    error(message: any, trace: string) {
+        this.logger.error(message, trace);
+    }
+
+    warn(message: any) {
+        this.logger.warn(message);
+    }
+
+    info(message: any) {
+        this.logger.info(message);
+    }
+
+    debug(message: any) {
+        this.logger.debug(message);
+    }
+
+    log(message: any) {
+        this.logger.log(message);
+    }
+}
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -3,7 +3,6 @@ import {
     Controller,
     ForbiddenException,
     Get,
-    Logger,
     Post,
     Request,
 } from '@nestjs/common';
@@ -19,19 +18,23 @@ import { UserFromAuth } from '@/common/decorator/user-from-auth.decorator';
 import { sha1 } from '@powerfulyang/node-utils';
 import { UserDto } from '@/entity/dto/UserDto';
 import { Profile } from 'passport-google-oauth20';
+import { AppLogger } from '@/common/logger/app.logger';
 
 @Controller('user')
 export class UserController {
-    private readonly logger = new Logger(UserController.name);
-
     constructor(
         @InjectRepository(User) private userDao: Repository<User>,
         private jwtService: JwtService,
-    ) {}
+        private logger: AppLogger,
+    ) {
+        this.logger.setContext(UserController.name);
+    }
 
     @Get('google/auth')
     @GoogleAuthGuard()
-    async googleAuth() {}
+    async googleAuth() {
+        this.logger.debug('user try login with google account!!!');
+    }
 
     @Get('google/auth/callback')
     @GoogleAuthGuard()
@@ -41,6 +44,7 @@ export class UserController {
 
     @Post('login')
     async login(@Body() user: UserDto) {
+        this.logger.info(`${user.email} try to login in!!!`);
         const { email, password } = user;
         const userInfo = await this.userDao.findOneOrFail({
             email,
