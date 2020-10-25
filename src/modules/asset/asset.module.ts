b79d007cbe09a7acd88b0a943fb8e43a140b1054
powerfulyang <i@powerfulyang.com>
2020-10-25 23:52:55 +0800

    feat: it's ok to login with google account~

--- src/modules/asset/asset.module.ts
+++ src/modules/asset/asset.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Asset } from '@/entity/asset.entity';
 import { AssetService } from './asset.service';
 import { AssetController } from './asset.controller';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Asset])],
     providers: [AssetService],
     controllers: [AssetController],
 })
