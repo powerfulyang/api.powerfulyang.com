bc556ba21711f42d16424d3dcc972767bfa20e20
powerfulyang <i@powerfulyang.com>
2020-11-09 02:28:02 +0800

    chore(:haircut:): strict mode in development

--- src/common/interceptor/cookie.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CookieInterceptor } from './cookie.interceptor';
-
-describe('CookieInterceptor', () => {
-    it('should be defined', () => {
-        expect(new CookieInterceptor()).toBeDefined();
-    });
-});
