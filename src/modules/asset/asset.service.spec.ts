f9a2cb55f5a239696c00ca7f8ac45cacbddda1fc
powerfulyang <i@powerfulyang.com>
2020-11-15 22:10:32 +0800

    feature(:macau:): map assets pHash

--- src/modules/asset/asset.service.spec.ts
+++ src/modules/asset/asset.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssetService } from './asset.service';
+import { AppModule } from '@/app.module';
 
 describe('AssetService', () => {
   let service: AssetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssetService],
+      imports: [AppModule],
     }).compile();
 
     service = module.get<AssetService>(AssetService);
@@ -14,5 +15,10 @@ describe('AssetService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('pHashMap', async function () {
+    const maps = await service.pHashMap();
+    expect(maps).toBeDefined();
   });
 });
