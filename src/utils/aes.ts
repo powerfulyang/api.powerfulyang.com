9db0448b7b0cda88f90ee639d1cb10ca0b2aa4e0
powerfulyang <i@powerfulyang.com>
2020-11-17 21:52:52 +0800

    todo(:tomato:) prepare to design my blog~

---
+++ src/utils/aes.ts
@@ -0,0 +1,24 @@
+import crypto from 'crypto';
+
+const algorithm = 'aes-128-cbc';
+
+const encrypt = (key: string, iv: string, data: string) => {
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(data, 'utf8', 'binary');
+  encrypted += cipher.final('binary');
+  return Buffer.from(encrypted, 'binary').toString('base64');
+};
+
+const decrypt = (key: string, iv: string, encrypted: string) => {
+  const toDecrypt = Buffer.from(encrypted, 'base64').toString('binary');
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decoded = decipher.update(toDecrypt, 'binary', 'utf8');
+  decoded += decipher.final('utf8');
+  return decoded;
+};
+
+export class AES {
+  static encrypt = encrypt;
+
+  static decrypt = decrypt;
+}
