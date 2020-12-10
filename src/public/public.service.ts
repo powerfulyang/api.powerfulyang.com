a43c6cc8f91429ac7109beb66235a4c371869db0
powerfulyang <i@powerfulyang.com>
2020-12-10 22:28:25 +0800

    fix: path view count and show post tags.

--- src/public/public.service.ts
+++ src/public/public.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { AppLogger } from '@/common/logger/app.logger';
+import { PostService } from '@/modules/post/post.service';
+import { Posts } from '@/entity/posts.entity';
 
 @Injectable()
-export class PublicService {}
+export class PublicService {
+  constructor(private logger: AppLogger, private postService: PostService) {
+    this.logger.setContext(PublicService.name);
+  }
+
+  getAllPublicPosts() {
+    return this.postService.publicList();
+  }
+
+  getPublicPostById(post: Posts) {
+    return this.postService.publicRead(post);
+  }
+}
