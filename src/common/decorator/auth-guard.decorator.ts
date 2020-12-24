a4b92055c82c405e7ad15be01bcc3bf33958126b
powerfulyang <i@powerfulyang.com>
2020-12-24 22:06:04 +0800

    perf(:deciduous_tree:): 调整样式哦

--- src/common/decorator/auth-guard.decorator.ts
+++ src/common/decorator/auth-guard.decorator.ts
@@ -1,5 +1,7 @@
 import { AuthGuard } from '@nestjs/passport';
 import { UseGuards } from '@nestjs/common';
+import { AdminGuard } from '@/common/decorator/admin-guard.decorator';
 
 export const JwtAuthGuard = () => UseGuards(AuthGuard('jwt'));
 export const GoogleAuthGuard = () => UseGuards(AuthGuard('google'));
+export const AdminAuthGuard = () => UseGuards(AdminGuard);
