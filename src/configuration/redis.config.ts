6f562c26d94918e8036c7a088a2a5b001900e2e4
powerfulyang <i@powerfulyang.com>
2020-12-26 22:57:30 +0800

    perf(:deciduous_tree:): configuration.

--- src/configuration/redis.config.ts
+++ src/configuration/redis.config.ts
@@ -2,6 +2,6 @@ export const redisConfig = () => {
   return {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-    auth_pass: process.env.REDIS_PASS,
+    auth_pass: process.env.REDIS_PASS || undefined,
   };
 };
