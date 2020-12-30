2295833c92cc5e70129bfc8ab785ae2be7fa7f48
powerfulyang <i@powerfulyang.com>
2020-12-30 18:28:30 +0800

    feat(:see_no_evil:): socks5 agent.

--- jest.config.js
+++ jest.config.js
@@ -2,21 +2,21 @@ const tsconfig = require('./tsconfig.json');
 const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig);
 
 module.exports = {
-    verbose: true,
-    displayName: {
-        name: 'api.powerfulyang.com.test',
-        color: 'green',
-    },
-    moduleNameMapper,
-    maxConcurrency: 5,
-    moduleFileExtensions: ['js', 'json', 'ts', 'node'],
-    setupFiles: ['./.jest/setEnvVars.ts'],
-    setupFilesAfterEnv: ['./jest.setup.ts'],
-    rootDir: '.',
-    testRegex: '.spec.ts$',
-    transform: {
-        '^.+\\.(t|j)s$': 'ts-jest',
-    },
-    testPathIgnorePatterns: ['node_modules'],
-    testEnvironment: 'node',
+  verbose: true,
+  displayName: {
+    name: 'api.powerfulyang.com.test',
+    color: 'green',
+  },
+  moduleNameMapper,
+  maxConcurrency: 5,
+  moduleFileExtensions: ['js', 'json', 'ts', 'node'],
+  setupFiles: ['./.jest/setEnvVars.ts'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  rootDir: '.',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['node_modules'],
+  testEnvironment: 'node',
 };
