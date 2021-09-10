30d1ecd97c7a2ec73b81a78bb85855ac87f947b0
powerfulyang <i@powerfulyang.com>
2021-09-10 18:31:43 +0800

    feat: 试用一下 node-addon-api

---
+++ __test__/TinyEXIF.spec.ts
@@ -0,0 +1,12 @@
+import { getEXIF } from '../addon.api';
+import { join } from 'path';
+import sharp from 'sharp';
+
+describe('C艹', () => {
+  it('getEXIF', async () => {
+    const path = join(process.cwd(), 'assets', 'test.jpg');
+    const metadata = await sharp(path).metadata();
+    const width = getEXIF(path);
+    expect(metadata.width).toBe(width);
+  });
+});
