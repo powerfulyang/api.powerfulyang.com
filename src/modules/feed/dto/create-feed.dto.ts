c7165a5c976ac27572ca5694044fe7807072e75c
powerfulyang <i@powerfulyang.com>
2021-10-27 16:59:17 +0800

    feat: new version linter

--- src/modules/feed/dto/create-feed.dto.ts
+++ src/modules/feed/dto/create-feed.dto.ts
@@ -1,8 +1,4 @@
-import { IsNotEmpty } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { Feed } from '@/modules/feed/entities/feed.entity';
 
-export class CreateFeedDto extends PartialType(Feed) {
-  @IsNotEmpty()
-  declare content: string;
-}
+export class CreateFeedDto extends PartialType(Feed) {}
--- src/modules/user/dto/PostDto.ts
+++ src/modules/user/dto/PostDto.ts
@@ -1,10 +1,3 @@
-import { Length } from 'class-validator';
 import { Post } from '@/modules/post/entities/post.entity';
 
-export class PostDto extends Post {
-  @Length(1, 100)
-  declare title: string;
-
-  @Length(1, 10000)
-  declare content: string;
-}
+export class PostDto extends Post {}
