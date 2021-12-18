4458651e2b13fffaea111319ba2725a46b3c2999
powerfulyang <i@powerfulyang.com>
2021-12-18 18:11:46 +0800

    feat: validator

--- src/modules/feed/dto/update-feed.dto.ts
+++ src/modules/feed/dto/update-feed.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty } from 'class-validator';
 import { CreateFeedDto } from './create-feed.dto';
 
-export class UpdateFeedDto extends PartialType(CreateFeedDto) {}
+export class UpdateFeedDto extends PartialType(CreateFeedDto) {
+  @IsNotEmpty()
+  declare content: string;
+}
