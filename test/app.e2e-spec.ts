5cf2d5493c7bb0b65c96f9785b6171aaa9722f22
powerfulyang <powerfulyang@qq.com>
2022-04-05 15:00:03 +0800

    fix: typo; cookie => cookies

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,16 @@
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
 import type { INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 import request from 'supertest';
+import { parse } from 'cookie';
 import { AppModule } from '@/app.module';
+import { UserService } from '@/modules/user/user.service';
+import { Authorization } from '@/constants/constants';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let userService: UserService;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,15 +18,39 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    userService = moduleFixture.get<UserService>(UserService);
     await app.init();
   });
 
   it('/public/hello (GET)', () => {
     return request(app.getHttpServer())
       .get('/public/hello')
-      .expect(200)
+      .expect(HttpStatus.OK)
       .expect((r) => {
         expect(r.body.data).toBe('Hello World!!!');
+      });
+  });
+
+  it('/user/login (POST)', async () => {
+    const user = await userService.getSaltByEmail('powerfulyang');
+    return request(app.getHttpServer())
+      .post('/user/login')
+      .send({
+        email: user.email,
+        password: user.salt,
+      })
+      .expect(HttpStatus.CREATED)
+      .expect(async (r) => {
+        const { header } = r;
+        const cookies = header['set-cookie'] as string[];
+        const authorization = cookies
+          .filter((cookie) => cookie.includes(Authorization))
+          .find((cookie) => cookie);
+        expect(authorization).toBeDefined();
+        const parsed = parse(authorization!) as { [Authorization]: string };
+        expect(parsed).toBeDefined();
+        const verified = await userService.verifyAuthorization(parsed[Authorization]);
+        expect(verified.id).toBe(user.id);
       });
   });
 });
