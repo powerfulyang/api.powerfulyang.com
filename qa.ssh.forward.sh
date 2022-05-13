fd154f2e43c4b80bfbeebb6aa93be1867c451252
powerfulyang <i@powerfulyang.com>
2022-05-13 22:10:15 +0800

    feat: 修改了端口

--- qa.ssh.forward.sh
+++ qa.ssh.forward.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ssh -L 56782:localhost:56782 -L 58210:localhost:58210 \
+ssh -L 9200:localhost:9200 -L 56782:localhost:56782 \
  -L 58212:localhost:58212 -L 58211:localhost:58211 -NC dev
